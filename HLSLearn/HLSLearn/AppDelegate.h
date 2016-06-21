36282787afdd7d1f6a2603134fcc9a29022ba84d
郭玉泽 <zezefamily@163.com>
2016-06-21 18:06:46 +0800

    HLS框架学习

---
+++ HLSLearn/HLSLearn/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HLSLearn
+//
+//  Created by 郭玉泽 on 16/6/21.
+//  Copyright © 2016年 郭玉泽. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
